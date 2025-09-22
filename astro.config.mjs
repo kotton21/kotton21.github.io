2a9cc056ade93547fe0d7367d2c3baa7bf9fb354
Karl Bayer <karlsbayer@gmail.com>
2025-09-22 09:21:44 -0500

    take 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   integrations: [mdx()],
   site: 'https://kotton21.github.io/astro-portfolio/', // Your GitHub Pages URL
-  base: process.env.NODE_ENV === 'production' ? '/astro-portfolio/' : '/', // Only use base path in production
+  base: '/astro-portfolio', // Use base path for both dev and production
   vite: {
     build: {
       rollupOptions: {

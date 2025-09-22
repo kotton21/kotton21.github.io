bda0cbe24f4b7b46d0852decc8e5382fe5d23faf
Karl Bayer <karlsbayer@gmail.com>
2025-09-22 08:40:37 -0500

    switched site url back to astro-portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://kotton21.github.io/astro-portfolio-fresh/', // Your GitHub Pages URL
-  base: process.env.NODE_ENV === 'production' ? '/astro-portfolio-fresh/' : '/', // Only use base path in production
+  site: 'https://kotton21.github.io/astro-portfolio/', // Your GitHub Pages URL
+  base: process.env.NODE_ENV === 'production' ? '/astro-portfolio/' : '/', // Only use base path in production
   vite: {
     build: {
       rollupOptions: {
